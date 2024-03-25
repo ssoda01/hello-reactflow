6e71cfb6071683e254b761bf6e11fcee1f35db87
ssoda01 <1443898356@qq.com>
2024-03-26 00:01:59 +0800

    draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import type { OnConnect } from "reactflow";
-
-import { useCallback } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
+import html2canvas from "html2canvas";
 import {
   Background,
   Controls,
@@ -9,21 +9,57 @@ import {
   addEdge,
   useNodesState,
   useEdgesState,
+  Panel,
 } from "reactflow";
 
 import "reactflow/dist/style.css";
 
 import { initialNodes, nodeTypes } from "./nodes";
 import { initialEdges, edgeTypes } from "./edges";
+import { Handle, Position } from "reactflow";
 
+const handleStyle = { left: 10 };
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [nodes, , addNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect: OnConnect = useCallback(
-    (connection) => setEdges((edges) => addEdge(connection, edges)),
+    (connection) => {
+      console.log("onConnect: connection:", connection);
+      return setEdges((edges) => addEdge(connection, edges));
+    },
     [setEdges]
   );
+  // const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
 
+  const handlerScreenshot = () => {
+    console.log("screenshot");
+    takeScreenshot();
+    return null;
+  };
+  const handlerAddNode = () => {
+    console.log("add node");
+    addNodes((e: any[]) =>
+      e.concat({
+        id: (e.length + 1).toString(),
+        data: { label: `${name}` },
+        position: {
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+        },
+        style: { border: "10px solid #9999" },
+      })
+    );
+  };
+  function takeScreenshot() {
+    html2canvas(document.body).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      // const img = new Image();
+      const link = document.createElement("a");
+      link.href = imgData;
+      link.download = "screenshot.png";
+      link.click();
+    });
+  }
   return (
     <ReactFlow
       nodes={nodes}
@@ -35,7 +71,18 @@ export default function App() {
       onConnect={onConnect}
       fitView
     >
-      <Background />
+      <Panel position="top-left">
+        <button type="button" onClick={handlerScreenshot}>
+          Export as Image
+        </button>
+      </Panel>
+      <Panel position="top-center">
+        <button type="button" onClick={handlerAddNode}>
+          Add Node
+        </button>
+      </Panel>
+
+      <Background variant={"cross"} color="pink" />
       <MiniMap />
       <Controls />
     </ReactFlow>
--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -1,5 +1,8 @@
 import type { Node, NodeTypes } from "reactflow";
+import { Handle, Position } from "reactflow";
 import { PositionLoggerNode } from "./PositionLoggerNode";
+import { useCallback } from "react";
+import TextUpdaterNode from "./TextUpdaterNode.tsx";
 
 export const initialNodes = [
   { id: "a", type: "input", position: { x: 0, y: 0 }, data: { label: "wire" } },
@@ -16,9 +19,16 @@ export const initialNodes = [
     position: { x: 0, y: 200 },
     data: { label: "with React Flow" },
   },
+  {
+    id: "e",
+    type: "text-updater",
+    position: { x: 0, y: 200 },
+    data: { label: "with React Flow" },
+  },
 ] satisfies Node[];
 
 export const nodeTypes = {
   "position-logger": PositionLoggerNode,
+  "text-updater": TextUpdaterNode,
   // Add any of your custom nodes here!
 } satisfies NodeTypes;
